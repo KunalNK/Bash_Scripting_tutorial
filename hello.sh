b1d3284732df520eca963396ab69b70dfbf45661
Kunal K <knkillekar@gmail.com>
2020-06-27 22:48:19 +0530

    updated hello world script

--- hello.sh
+++ hello.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "Hello world"
+
+echo $BASH     # Bash name
+echo $BASH_VERSION   # To display bash version
+echo $HOME   # To display home directory
+echo $PWD   # To display present working directory
